e38414ab54faae8424b62ef22750567ff8b0ba56
237programming <237programming@gmail.com>
2016-01-26 21:00:34 -0500

    vision works for now!

---
+++ src/org/usfirst/frc/team237/robot/commands/TrackTarget.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team237.robot.commands;
+
+import org.usfirst.frc.team237.robot.Robot;
+import org.usfirst.frc.team237.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TrackTarget extends Command {
+
+    public TrackTarget() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    	System.out.println("INITIALIZING TRACKING COMMAND \n");
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.driveTrain.visionStart();
+    	System.out.println("DONE INITIALIZING COMMAND \n");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	System.out.println("DONE WITH VISION COMMAND \n");
+    	Robot.driveTrain.visionStop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
